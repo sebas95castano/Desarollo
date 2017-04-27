b9872f7571c6fe19f8213b557d29baddf688ef0f
root <root@localhost.localdomain>
2017-04-27 01:55:03 -0400

    Implementacion de suma

---
+++ TallerGitNetbeans.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallergitnetbeans;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class TallerGitNetbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculadoraSimple interfaz = new CalculadoraSimple();
+        interfaz.setVisible(true);
+    }
+    
+}
